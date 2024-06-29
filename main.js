b1803f853ff731b3d2e7468141bc814dc2437fa8
saifur550 <srstap550@gmail.com>
2024-06-29 12:48:34 +0600

    js problem

--- main.js
+++ main.js
@@ -1,3 +1,60 @@
+let hello =  'https://www.youtube.com/watch?v=3l3gqpxxQpY'
+
+let greet = `My Js ${hello}`;
+//console.log(greet);
+
+
+
+// ! print the numbers from 1 to 5 to the console.log using the loop
+
+for ( 
+  let aa = 0 ;
+      aa<=50; 
+      aa+=2){
+  let result  = aa++
+  //console.log(result);
+}
+
+
+
+
+let xx = 0 
+while(xx<=10){
+  //console.log(xx);
+  xx++
+}
+
+
+let num1 = 0 ;
+
+do {
+   //console.log(num1) 
+   num1++
+  }
+while(num1<1) 
+
+let fewArray = Array.from('1234', Number)
+//console.log(fewArray);
+
+
+
+let r = ('1-2-3'.split('-'));
+console.log(r);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // initialize array
 const  arr1 = [
@@ -8,13 +65,13 @@ const  arr1 = [
 
   arr1.push('new')
 
-  console.log(arr1);
+  //console.log(arr1);
   
 
   //display all array 
 
   for ( let x = 0 ; x<arr1.length; x++){
-    console.log(arr1[x]);
+    //console.log(arr1[x]);
   }
 
 
@@ -33,8 +90,8 @@ const  arr1 = [
     ...arr,
   ];
   
-  console.log(newArr1, newArr2);
+  //console.log(newArr1, newArr2);
 
 
   const newArr3 = arr.concat(newArr1)
-  console.log(newArr3);
+  //console.log(newArr3);
